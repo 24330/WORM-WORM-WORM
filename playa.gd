813e3c28c28462aef12bb78cb52df6cb7e84c56d
24330 <24330@burnside.school.nz>
2026-05-28 14:40:41 +1200

    I added eat time

--- playa.gd
+++ playa.gd
@@ -20,5 +20,6 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 
-func _on_foods_area_entered(area: Area2D) -> void:
-	$"../foods".hide()
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("food"):
+		area.queue_free()
