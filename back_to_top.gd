918518d15557bfe85358746e2f85fb46f9e29160
24330 <24330@burnside.school.nz>
2026-07-31 09:18:31 +1200

    lotsa work
    fixed the restart button, added level 2, dont die always now

--- back_to_top.gd
+++ back_to_top.gd
@@ -1,5 +1,4 @@
-extends Area2D
-
+extends StaticBody2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -10,13 +9,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
 
-
-
-func _on_area_entered(area: Area2D) -> void:
+func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player"):
-		GlobalDirt.dirt_dere -= 1
-		queue_free()
-		if GlobalDirt.dirt_dere == 0:
-			get_tree().change_scene_to_file("res://Level 2.tscn")
-			GlobalDirt.dirt_dere = 300
+		area.get_parent().respawn()
--- play_again_button.gd
+++ play_again_button.gd
@@ -10,5 +10,6 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-func _on_button_down() -> void:
+
+func _on_pressed() -> void:
 	get_tree().change_scene_to_file("res://main_Scene.tscn")
