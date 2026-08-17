1f5490d38b2ef46cf996a2a98c98f983799118a6
24330 <24330@burnside.school.nz>
2026-08-18 09:27:54 +1200

    WORM COOL FUCKONG GAME FUCK

--- foods.gd
+++ foods.gd
@@ -17,9 +17,11 @@ func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player"):
 		GlobalDirt.dirt_dere -= 1
 		queue_free()
-		if GlobalDirt.dirt_dere == 0:
+		if GlobalDirt.scene == 1 and  GlobalDirt.dirt_dere == 0:
 			get_tree().change_scene_to_file("res://Level 2.tscn")
 			GlobalDirt.dirt_dere = 300
-		if scene_file_path == ("res://Level 2.tscn") and GlobalDirt.dirt_dere == 0:
-			get_tree().change_scene_to_file("res://level 3.tscn")
+			GlobalDirt.scene += 1
+		if GlobalDirt.scene == 2 and GlobalDirt.dirt_dere == 0:
+			get_tree().change_scene_to_file("res://You_win.tscn")
 			GlobalDirt.dirt_dere = 300
+			GlobalDirt.scene += 1
--- global_dirt.gd
+++ global_dirt.gd
@@ -2,3 +2,4 @@ extends Node
 
 var dirt_dere = 300
 var lives = 10
+var scene = 1
